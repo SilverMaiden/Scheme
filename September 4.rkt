50ee7c466f897b6b769b5250674c95141ce2eb06
Humaira <h.specials@gmail.com>
2014-09-06 17:42:19 -0700

    initial commit chapter 2 to 2.4

---
+++ September 4.rkt
@@ -0,0 +1,109 @@
+(load "functions.scm")
+(functions)
+> (load "functions.scm")
+> (functions)
+butfirst
+Welcome to the FUNCTIONS program.
+Function: butfirst
+Argument: a
+
+The result is: 
+
+Function: count
+Argument: 765432
+
+The result is: 6
+
+Function: +
+Argument: seven
+Argument: 2
+Argument(s) not in domain.
+Function: butfirst
+Argument: a
+
+The result is: 
+
+Function: count 
+Argument: 765432
+
+The result is: 6
+
+Function: expt
+Argument: -3
+Argument: .5
+Argument(s) not in domain.
+Function: remainder
+Argument: 5
+Argument: 0
+Argument(s) not in domain.
+Function: sentence
+Argument: (when i get)
+Argument: home
+
+The result is: (when i get home)
+
+Function: butfirst
+Argument: (yet blues)
+
+The result is: (blues)
+
+Function: butfirst
+Argument: (cake)
+
+The result is: ()
+
+Function: cake
+Sorry, that's not a function.
+Function: butfirst
+Argument: cake
+
+The result is: ake
+
+Function: butlast
+Argument: cake
+
+The result is: cak
+
+Function: butlast
+Argument: (cake stroodle pastry)
+
+The result is: (cake stroodle)
+
+Function: butlast
+Argument: cake stroodle pasty
+You typed more than one argument!  Try again.
+Argument: s
+
+The result is: 
+
+Function: every
+Argument: first
+Argument: (the long and winding road)
+
+The result is: (t l a w r)
+
+Function: every
+Argument: second
+Argument: (the long and winding road)
+Argument(s) not in domain.
+Function: every
+Argument: last
+Argument: (the long and winding road)
+
+The result is: (e g d g d)
+
+Function: keep
+Argument: vowel?
+Argument: constantinople
+
+The result is: oaioe
+
+Function: keep
+Argument: even
+Argument: 3
+Argument(s) not in domain.
+Function: keep
+Argument: even?
+Argument: 12345678
+
+The result is: 2468
